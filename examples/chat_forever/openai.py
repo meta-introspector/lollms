9e900624739b936f871e88d6bef3fc5e51bad2ba
jmikedupont2 <jmikedupont2@gmail.com>
2023-09-27 10:42:16 +0000

    working better

--- examples/chat_forever/openai.py
+++ examples/chat_forever/openai.py
@@ -3,34 +3,19 @@ import sys
 from  ai_ticket.events.inference import on_event
 import time
 maxtry=10
-#import dirtyjson
-#import streamlit as st
 from collections import deque
 from pathlib import Path
 import json
 import re
 import random
 import pwd, os
-#import personas
 from flask import Flask, make_response, request, abort
 from flask.json import jsonify
-
 DEBUG = 0
 BUNDLES = 4
-
-
-
-
 username = pwd.getpwuid(os.getuid()).pw_name
-
-
-# set up the Flask application
 app = Flask(__name__)
-
-#if __name__ == "__main__":
-
 models = {}
-
 
 @app.route("/v1/models", methods=['GET'])
 def models():
@@ -44,64 +29,6 @@ def models():
     ]
     return {'data': data, 'object': 'list'}
 
-# @app.route("/v1/engines/<model_name>/completions", methods=["POST"])
-# def completions(model_name):
-#     # get the request data
-#     #print("completions", request.text)
-#     data = request.get_json(force=True)
-#     # is it an alias?
-#     if (model_name in models):
-#         model_name = models[model_name]
-   
-#     # get the prompt and other parameters from the request data
-#     prompt = data["prompt"]
-#     max_tokens = data.get("max_tokens", 16)
-#     temperature = data.get("temperature", 1.0)
-#     top_p = data.get("top_p", 0.75)
-#     top_k = data.get("top_k", 40)
-#     num_beams = data.get("num_beams", 1)
-#     max_new_tokens = data.get("max_new_tokens", 256)
-
-#     #kwargs = decode_kwargs(data)
-    
-#     if (model_name in llamaModels):
-#         #generated_text = evaluate_llama(prompt,**kwargs)
-#         generated_text = evaluate_llama(prompt,
-#                                         #input = prompt,
-#                                         temperature=temperature,
-#                                         top_p=top_p,
-#                                         top_k=top_k,
-#                                         num_beams=num_beams,
-#                                         max_new_tokens=max_new_tokens,
-#                                         **kwargs)
-
-#     # else:
-#     #     input_ids = tokenizer.encode(prompt, return_tensors='pt')
-#     #     output = model.generate(input_ids=input_ids,
-#     #                             max_length=max_tokens, 
-#     #                             temperature=temperature,
-#     #                             top_p=top_p,
-#     #                             top_k=top_k,
-#     #                             **kwargs)
-#     #     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-#     # prompt_tokens = len(tokenizer.encode(prompt))
-#     # completion_tokens = len(tokenizer.encode(generated_text))
-#     # total_tokens = prompt_tokens + completion_tokens
-#     # return jsonify( {
-#     #         'object': 'text_completion',
-#     #         'id': 'dummy',
-#     #         'created': int(time.time()),
-#     #         'model': model_name,
-#     #         'choices': 
-#     #             [{'text': generated_text, 'finish_reason': 'length'}],
-#     #         'usage': {
-#     #                 'prompt_tokens': prompt_tokens,
-#     #                 'completion_tokens': completion_tokens,
-#     #                 'total_tokens': total_tokens
-#     #                 }
-#     #             }
-#     #         )
     
 @app.route("/v1/chat/completions", methods=["POST"])
 def chat_completions():
@@ -118,10 +45,6 @@ def chat_completions():
         prompt += message["role"] + ": " + message["content"] + "\n"
     #prompt += "assistant: "
 
-    # is it an alias?
-    #if (model_name in models):
-    #    model_name = models[model_name]
-   
     # get the prompt and other parameters from the request data
     #prompt = data["prompt"]
     max_tokens = data.get("max_tokens", 16)
@@ -172,20 +95,17 @@ def chat_completions():
     # args: Record<string, any>;
     # };
     # }
+    
+    # create a ticket for the request
     ticket = on_event({"content" :json.dumps(data,indent=2) })
     print("TICKET",ticket)
 
     ticket_url = ticket
 
-    
     jsondata= json.dumps({
         "command": {
             "name": "request_assistance",
-            "args": {
-                "ticket_url": ticket_url,
-                "next_action": "poll_url"
-            }
-        },
+            "args": {        "ticket_url": ticket_url,     "next_action": "poll_url"            }        },
         "thoughts": {
             "plan": "Initiated a request for assistance.",
             "speak": output1,
@@ -193,84 +113,69 @@ def chat_completions():
             "reasoning" : "todo",
             "text": f"I encountered an issue with our application, and I need assistance. I've created a ticket for it. Here's the URL to the ticket: {ticket_url}. My next action is to poll that URL for updates."
         } })
-    
-    output_data = f"""```{jsondata}```"""
 
-    data2= {
-        'id': 'chatcmpl-%s' % completion_id,
-            'object': 'chat.completion',
-            'created': completion_timestamp,
-            'model': model_name,
-            'usage': {
-                'prompt_tokens': 0,
-                'completion_tokens': 0,
-                'total_tokens': 0
+    #this  is the mock json data from the llm itself
+    output_data = jsondata#f"""```{jsondata}```"""
+
+    ##### this is the new ticket for the user to replace
+    data2 =   {
+            "command": {
+                "name": "execute_python_code",
+                "args" : { "code": "print(42)" },
             },
-                "choices": [
+            "thoughts": {
+                "plan": "User is to replace this.",
+                "speak": "This is where you speak to the requesting user. Replace the command above and this text with your results",
+                "criticism": "todo",
+                "reasoning" : "todo",
+                "text": f"This template will be executed."
+            }}
+    # create a second ticket to handle the agent
+    ticket2 = on_event({"content" : json.dumps(data2) })
+    ticket_url = ticket2
+
+
+    return {
+        'id': 'chatcmpl-%s' % completion_id,
+        'object': 'chat.completion',
+        'created': completion_timestamp,
+        'model': model_name,
+        'usage': {
+            'prompt_tokens': 0,
+            'completion_tokens': 0,
+            'total_tokens': 0
+        },
+        "choices": [
             {
-                "message" : { "content": output_data},
+                "message" : { "content": json.dumps({
+        "command": {
+            "name": "request_assistance",
+            "args": {        "ticket_url": ticket_url,     "next_action": "poll_url"            }        },
+        "thoughts": {
+            "plan": "Initiated a request for assistance.",
+            "speak": output1,
+            "criticism": "todo",
+            "reasoning" : "todo",
+            "text": f"I encountered an issue with our application, and I need assistance. I've created a ticket for it. Here's the URL to the ticket: {ticket_url}. My next action is to poll that URL for updates."
+        } })},
             }
         ],
+        "thoughts": {
+            "plan": "Initiated a request for assistance.",
+            "speak": "asking for help",
+            "criticism": "todo",
+            "reasoning" : "todo",
+            "text": f"testing."
+        },
         "command": {
             "name": "request_assistance",
             "args": {
                 "ticket_url": ticket_url,
                 "next_action": "poll_url"
-            },
-            #"choices": [],
+            }
         }
     }
-    print("DEBUG",data2)
 
-    # create a second ticket to handle the agent
-    ticket2 = on_event({"content" :json.dumps(data2,indent=2) })
-        
-    return data2
-    #yield f'data: %s\n\n' % data2
-
-    #    return Response(inter(msg, messages), mimetype="text/event-stream")
-    def stream():
-        # taken from FreeGPT/endpoint.py
-        #{'role':'assistant','content': , 'finish_reason': 'stop'}]
-        
-        completion_data = {
-            'id': '',
-            'object': 'chat.completion.chunk',
-            'created': 0,
-            'model': model_name,
-            'choices': [
-                {
-                    'delta': {
-                        'content': ""
-                    },
-                    'index': 0,
-                    'finish_reason': None
-                }
-            ]
-        }
-
-        for token in generated_text.split():
-            completion_id = ''.join(
-                random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', k=28))
-            completion_timestamp = int(time.time())
-            completion_data['id'] = f'chatcmpl-{completion_id}'
-            completion_data['created'] = completion_timestamp
-            completion_data['choices'][0]['delta']['content'] = token +" "
-            if token.startswith("an error occured"):
-                completion_data['choices'][0]['delta']['content'] = "Server Response Error, please try again.\n"
-                completion_data['choices'][0]['delta']['stop'] = "error"
-                #yield 'data: %s\n\ndata: [DONE]\n\n' % json.dumps(completion_data, separators=(',' ':'))
-                return
-            yield 'data: %s\n\n' % json.dumps(completion_data, separators=(',' ':'))
-            time.sleep(0.1)
-
-        #completion_data['choices'][0]['finish_reason'] = "stop"
-        #completion_data['choices'][0]['delta']['content'] = ""
-        
-        #yield 'data: %s\n\n' % json.dumps(completion_data, separators=(',' ':'))
-        #yield 'data: [DONE]\n\n'
-
-    return app.response_class(stream(), mimetype='text/event-stream')
 
     
 
