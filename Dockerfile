57a29f5141fbac2ebe6ce1fe691a150ca7cb08bf
mike dupont <mike.dupont@introspector.local>
2023-10-11 18:08:09 -0400

    adding controller

--- Dockerfile
+++ Dockerfile
@@ -7,13 +7,14 @@ FROM ${BASE_IMAGE}
 WORKDIR /app
 
 # Copy the current directory contents into the container at /app
-COPY . /app
+COPY requirements.txt /app/requirements.txt
 
 # Install any needed packages specified in requirements.txt
 RUN apt update
 RUN apt install -y git
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
+COPY . /app/
 # Run app.py when the container launches
 CMD ["python", "-m", "examples.chat_forever.openai","--host","0.0.0.0"]
 
--- examples/chat_forever/docker_controller.py
+++ examples/chat_forever/docker_controller.py
@@ -1,11 +1,49 @@
 import docker
+import json
 
 def inspect():
     client = docker.from_env()
     data = {}
+    keys = [] #any secrets we found
     for container in client.containers.list():
-        data[container.name] = dict(
-            attrs = str(container.attrs),
+        attrs = {}
+        for x in container.attrs:
+            #print("attr",x)
+            if x == "Config":
+                attrs[x] = container.attrs[x]
+                config = container.attrs[x]
+                envs = config["Env"]
+                #print(envs)
+                clean = []
+                for y in envs:
+                    #print(y)
+                    for z in ["KEY", "GITHUB_PAT", "SECRET","TOKEN" , "PASS"]:
+                        if z in y.upper():                        
+                            parts = y.split("=")
+                            keys.append(parts[1])
+                            parts[1]="redacted"
+                            y = "=".join(parts)
+                    clean.append(y)
+                config["Env"] = clean
+                attrs[x]["Env"] = clean
+            else:
+                attrs[x] = container.attrs[x]
+
+            #print(x)
+
+        #attrs2 = container.attrs
+        #attrs2['envs'] = envs
+        #pdb.set_trace()
+        #print( attrs['Config']['Env'])
+        #print( keys)
+        toclean = json.dumps(dict(
+            attrs = attrs,
             logs = str(container.logs())
-            )
+            ))
+        for k in keys:
+            toclean = toclean.replace(k,"redacted2")
+            
+        data[container.name] = toclean #only clean data
         return data
+if __name__ == "__main__":
+    print(inspect())
